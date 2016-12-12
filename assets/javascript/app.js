a64b6da3096d35c50b160db34e7c4fde75b6b894
megan liken <meganliken@megans-MacBook-Pro-2.local>
2016-12-11 19:21:23 -0600

    table google pins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 var colors = ["#B8D430", "#3AB745", "#029990", "#3501CB",
-             "#2E2C75", "#673A7E", "#CC0071", "#F80120",
-             "#F35B20", "#FB9A00", "#FFCC00", "#FEF200","#FB9A00", "#FFCC00"];
-var restaurants = ["Korean", "Indian", "Italian", "Sandwiches","Burgers", "Breakfast",
-                   "Mexican", "Caribbean","Vietnamese", "Chinese",
-                   "Seafood", "Pizza", "Thai", "Japanese"];
+    "#2E2C75", "#673A7E", "#CC0071", "#F80120",
+    "#F35B20", "#FB9A00", "#FFCC00", "#FEF200", "#FB9A00", "#FFCC00"
+];
+var restaurants = ["Korean", "Indian", "Italian", "Sandwiches", "Burgers", "Breakfast",
+    "Mexican", "Caribbean", "Vietnamese", "Chinese",
+    "Seafood", "Pizza", "Thai", "Japanese"
+];
 var startAngle = 0;
 var arc = Math.PI / 6;
 var spinTimeout = null;
@@ -12,107 +14,110 @@ var spinTime = 0;
 var spinTimeTotal = 0;
 var ctx;
 
-var foodnum=[];
- console.log(foodnum);
- $("#spin").on("click", spin);
-    
+var foodnum = [];
+console.log(foodnum);
+$("#spin").on("click", spin);
+
 function drawRouletteWheel() {
-  var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    var outsideRadius = 200;
-    var textRadius = 160;
-    var insideRadius = 125;
-   
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0,0,500,500);
-   
-   
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-   
-    ctx.font = 'bold 12px Helvetica, Arial';
-   
-    for(var i = 0; i < 12; i++) {
-      var angle = startAngle + i * arc;
-      ctx.fillStyle = colors[i];
-     
-      ctx.beginPath();
-      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
-      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-      ctx.stroke();
-      ctx.fill();
-     
-      ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur    = 0;
-      ctx.shadowColor   = "rgb(220,220,220)";
-      ctx.fillStyle = "black";
-      ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
-                    250 + Math.sin(angle + arc / 2) * textRadius);
-      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-      var text = restaurants[i];
-      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-      ctx.restore();
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        var outsideRadius = 200;
+        var textRadius = 160;
+        var insideRadius = 125;
+
+        ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, 500, 500);
+
+
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+
+        ctx.font = 'bold 12px Helvetica, Arial';
+
+        for (var i = 0; i < 12; i++) {
+            var angle = startAngle + i * arc;
+            ctx.fillStyle = colors[i];
+
+            ctx.beginPath();
+            ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+            ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+            ctx.stroke();
+            ctx.fill();
+
+            ctx.save();
+            ctx.shadowOffsetX = -1;
+            ctx.shadowOffsetY = -1;
+            ctx.shadowBlur = 0;
+            ctx.shadowColor = "rgb(220,220,220)";
+            ctx.fillStyle = "black";
+            ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
+                250 + Math.sin(angle + arc / 2) * textRadius);
+            ctx.rotate(angle + arc / 2 + Math.PI / 2);
+            var text = restaurants[i];
+            ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+            ctx.restore();
+        }
+
+        //Arrow
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+        ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+        ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
+        ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
+        ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
+        ctx.fill();
     }
-   
-    //Arrow
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
-    ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.fill();
-  }
 }
-   
-function spin() { 
-  spinAngleStart = Math.random() * 10 + 10;
-  spinTime = 0;
-  spinTimeTotal = Math.random() * 3 + 4 * 1000;
-  rotateWheel();
+
+function spin() {
+    spinAngleStart = Math.random() * 10 + 10;
+    spinTime = 0;
+    spinTimeTotal = Math.random() * 3 + 4 * 1000;
+    rotateWheel();
 }
+
 function rotateWheel() {
-  spinTime += 20;
-  if(spinTime >= spinTimeTotal) {
-    stopRotateWheel();
-    return;
-  }
-  var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-  startAngle += (spinAngle * Math.PI / 180);
-  drawRouletteWheel();
-  spinTimeout = setTimeout('rotateWheel()', 30);
+    spinTime += 20;
+    if (spinTime >= spinTimeTotal) {
+        stopRotateWheel();
+        return;
+    }
+    var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+    startAngle += (spinAngle * Math.PI / 180);
+    drawRouletteWheel();
+    spinTimeout = setTimeout('rotateWheel()', 30);
 }
+
 function stopRotateWheel() {
-  clearTimeout(spinTimeout);
-  var degrees = startAngle * 180 / Math.PI + 90;
-  var arcd = arc * 180 / Math.PI;
-  var index = Math.floor((360 - degrees % 360) / arcd);
-  ctx.save();
-  ctx.font = 'bold 30px Helvetica, Arial';
-  var text = restaurants[index]
-  ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
-  ctx.restore();
-  foodnum.push(index);
-  console.log(index);
-  console.log(foodnum);
-  yelpsearch(restaurants[index]);
+    clearTimeout(spinTimeout);
+    var degrees = startAngle * 180 / Math.PI + 90;
+    var arcd = arc * 180 / Math.PI;
+    var index = Math.floor((360 - degrees % 360) / arcd);
+    ctx.save();
+    ctx.font = 'bold 30px Helvetica, Arial';
+    var text = restaurants[index]
+    ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
+    ctx.restore();
+    foodnum.push(index);
+    console.log(index);
+    console.log(foodnum);
+    yelpsearch(restaurants[index]);
 }
+
 function easeOut(t, b, c, d) {
-  var ts = (t/=d)*t;
-  var tc = ts*t;
-  return b+c*(tc + -3*ts + 3*t);
+    var ts = (t /= d) * t;
+    var tc = ts * t;
+    return b + c * (tc + -3 * ts + 3 * t);
 }
 
 
 //==========google map API==========
 //create variable for coordinates
-var coordinates=[];
+var coordinates = [];
 var address;
 var map;
 var infoWindow;
@@ -120,51 +125,78 @@ var service;
 
 
 function geocodeAddress(geocoder, resultsMap) {
-        geocoder.geocode({'address': address}, function(results, status) {
-            if (status === 'OK') {
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
             coordinates.push(results[0].geometry.location);
             console.log(coordinates);
             var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
+                map: resultsMap,
+                position: results[0].geometry.location
             });
-          }
-        });
-      };
+        }
+    });
+};
 
 //
 function initMap() {
-      //create geocoder to get coordinates
-      var geocoder = new google.maps.Geocoder();
+    //create geocoder to get coordinates
+    var geocoder = new google.maps.Geocoder();
 
-      //add event listener to add coordinates to the empty array
-      $("#submit").on("click", function(){
-      address = document.getElementById('address').value;
-      geocodeAddress(geocoder, map);
-      return false;
-      });
+    //add event listener to add coordinates to the empty array
+    $("#submit").on("click", function() {
+        address = document.getElementById('address').value;
+        geocodeAddress(geocoder, map);
+        return false;
+    }); //end function initMap
 
-      //table appears here 
 
-        // Create a map object and specify the DOM element for display.
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 29.7051, lng: -95.4018},
-          scrollwheel: false,
-          zoom: 12
-        });
-
-    }
-
-function yelpsearch(restaurant){
-    var yelpURL = "http://localhost:5000/yelp/search?term=" + restaurant + "&location=houston";
-    $.ajax({url: yelpURL, method: "GET"
-          }).done(function(response){
-             console.log(response);
-             console.log(yelpURL);
-
-          
+    // Create a map object and specify the DOM element for display.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 29.7051, lng: -95.4018 },
+        scrollwheel: false,
+        zoom: 12
     });
-      }
+
+} //end map get element
+
+function yelpsearch(restaurant) {
+    var yelpURL = "http://localhost:5000/yelp/search?term=" + restaurant + "&location=houston";
+    $.ajax({
+        url: yelpURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+        console.log(yelpURL);
+
+        function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        } //end function callback 
+
+        function createMarker(place) {
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        } //end function createMarker
+
+    });
+} //end yelpsearch function
+
+
+//displays table here
+//ask for verification on syntax
+$('#hideTable').removeClass('hidden');
+
 $(document).ready(drawRouletteWheel, initMap);
 //table hidden
